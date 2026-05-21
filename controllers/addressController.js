49185c36b08e824a642b6b0b7e991e7f78b33c08
Milad salamian <miladsalami1385@gmail.com>
2026-05-22 01:24:52 +0330

    add try catch block in address controller

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -3,23 +3,35 @@ const addressService = require("../services/addressService");
 module.exports = {
   async addNewAddress(req, res) {
     const userId = req.user.id;
-    const address = await addressService.addAddress(req.body, userId);
+    try {
+      const address = await addressService.addAddress(req.body, userId);
 
-    return res.status(201).send({
-      statusCode: 201,
-      message: "آدرس با موفقیت ساخته شد",
-      data: address,
-    });
+      return res.status(201).send({
+        statusCode: 201,
+        message: "آدرس با موفقیت ساخته شد",
+        data: address,
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ statusCode: 500, message: "خطا وجود دارد", error });
+    }
   },
   async removeAddress(req, res) {
     const { id } = req.params;
     const userId = req.user.id;
 
-    const deletedAddress = await addressService.deleteAddressById(id, userId);
-    return res.status(200).send({
-      statusCode: 200,
-      message: "آدرس با موفقیت حذف شد",
-      data: deletedAddress,
-    });
+    try {
+      const deletedAddress = await addressService.deleteAddressById(id, userId);
+      return res.status(200).send({
+        statusCode: 200,
+        message: "آدرس با موفقیت حذف شد",
+        data: deletedAddress,
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ statusCode: 500, message: "خطا وجود دارد", error });
+    }
   },
 };
