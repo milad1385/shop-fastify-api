ab0ffb46a17d9a6340b6e1740a9678e10f4d9145
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 16:57:17 +0330

    add is admin middleware in users router

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,16 @@
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/isAdmin");
 
 function router(fastify, options) {
   fastify.post("/register", userController.register);
   fastify.post("/login", userController.login);
   fastify.get("/me", { preHandler: authMiddleware }, userController.getMe);
-  fastify.get("/", { preHandler: authMiddleware }, userController.getAllUsers);
+  fastify.get(
+    "/",
+    { preHandler: [authMiddleware, isAdmin] },
+    userController.getAllUsers,
+  );
 }
 
 module.exports = router;
