af3d9748bcbf7cd4b2d3e3bec426516c9ffff70b
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 16:49:46 +0330

    add auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const { User, Wallet } = require("../models");
+
+async function authMiddleware(req, res) {
+  try {
+    let token = req.headers.authorization;
+
+    if (!token)
+      return res
+        .status(401)
+        .send({ statusCode: 401, message: "لطفا توکن را وارد کنید" });
+
+    token = token.split(" ")[1];
+
+    // token validation
+    const decoded = jwt.verify(token, "secretkey");
+
+    const user = await User.findOne({
+      where: { id: decoded.id },
+      include: [
+        { model: Wallet, as: "wallet", attributes: { exclude: ["user_id"] } },
+      ],
+      attributes: { exclude: ["password"] },
+    });
+
+    console.log(decoded.id);
+
+    req.user = user;
+  } catch (error) {
+    return res
+      .status(401)
+      .send({ message: "توکن نامعتبر می باشد", statusCode: 401 });
+  }
+}
+
+module.exports = authMiddleware;
