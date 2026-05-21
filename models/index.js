1aa39e29ef27936aabfed2be9d092482c78d3a93
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 15:59:46 +0330

    add relation between wallet and user

--- models/index.js
+++ models/index.js
@@ -1,4 +1,8 @@
 const User = require("./User");
+const Wallet = require("./Wallet");
 const sequelize = require("../configs/db");
+
+User.hasOne(Wallet, { foreignKey: "user_id", as: "wallet" });
+Wallet.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 module.exports = { User, sequelize };
