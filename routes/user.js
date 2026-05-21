6ad752511a613ad88170c3bec1db14915b371df1
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 16:17:45 +0330

    add get me route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 function router(fastify, options) {
   fastify.post("/register", userController.register);
   fastify.post("/login", userController.login);
+  fastify.get("/me", { preHandler: authMiddleware }, userController.getMe);
 }
 
 module.exports = router;
