a3591846e4d9d9ec1e4c22748d7cb3f53c7906aa
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 13:42:49 +0330

    start project in server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const fastify = require("fastify")({ logger: true });
+const userRouter = require("./routes/user");
+
+fastify.register(
+  async (fastify, options) => {
+    fastify.register(userRouter, { prefix: "/users" });
+  },
+  { prefix: "/api/v1" },
+);
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log("server is running on ports 3000");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+start();
