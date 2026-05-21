7c15066301a96f3bcb2e68a47fed9404bf5397fe
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 16:50:12 +0330

    add get all user route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const userController = require("../controllers/userController");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 function router(fastify, options) {
   fastify.post("/register", userController.register);
   fastify.post("/login", userController.login);
   fastify.get("/me", { preHandler: authMiddleware }, userController.getMe);
+  fastify.get("/", { preHandler: authMiddleware }, userController.getAllUsers);
 }
 
 module.exports = router;
