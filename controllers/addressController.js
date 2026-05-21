b94a88cb3b0df2ba91b7e4df536a8981a567f6ff
Milad salamian <miladsalami1385@gmail.com>
2026-05-22 01:17:24 +0330

    add remove address and add new address controller

---
+++ controllers/addressController.js
@@ -0,0 +1,25 @@
+const addressService = require("../services/addressService");
+
+module.exports = {
+  async addNewAddress(req, res) {
+    const userId = req.user.id;
+    const address = await addressService.addAddress(req.body, userId);
+
+    return res.status(201).send({
+      statusCode: 201,
+      message: "آدرس با موفقیت ساخته شد",
+      data: address,
+    });
+  },
+  async removeAddress(req, res) {
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    const deletedAddress = await addressService.deleteAddressById(id, userId);
+    return res.status(200).send({
+      statusCode: 200,
+      message: "آدرس با موفقیت حذف شد",
+      data: deletedAddress,
+    });
+  },
+};
