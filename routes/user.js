ea7fdf387769fc21b82ecfab23a9051268aff1b8
Milad salamian <miladsalami1385@gmail.com>
2026-05-21 15:14:00 +0330

    add users route

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,8 @@
-function router(fastify, options) {}
+const userController = require("../controllers/userController");
+
+function router(fastify, options) {
+  fastify.post("/register", userController.register);
+  fastify.post("/login", userController.login);
+}
 
 module.exports = router;
