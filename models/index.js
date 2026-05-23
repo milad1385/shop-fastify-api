a46978c4641a8ec2a7b467338ad5411064c534fd
Milad salamian <miladsalami1385@gmail.com>
2026-05-23 16:03:20 +0330

    add relation between ban and user

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Wallet = require("./Wallet");
 const Address = require("./Address");
+const Ban = require("./Ban");
 const sequelize = require("../configs/db");
 
 // relation -> User , Wallet
@@ -30,4 +31,30 @@ Address.belongsTo(User, {
   as: "user",
 });
 
-module.exports = { User, Wallet , Address, sequelize };
+// relation -> User , Ban
+
+User.hasMany(Ban, {
+  foreignKey: "user_id",
+  as: "bans",
+  onDelete: "CASCADE",
+});
+
+Ban.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "banned_user",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Ban, {
+  foreignKey: "banned_by_id",
+  as: "bans_by_admin",
+  onDelete: "SET NULL",
+});
+
+Ban.belongsTo(User, {
+  foreignKey: "banned_by_id",
+  as: "banned_by_admin",
+  onDelete: "SET NULL",
+});
+
+module.exports = { User, Wallet, Address, Ban, sequelize };
