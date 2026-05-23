336c461d8e1264677ff324590a3fc60e3df03066
Milad salamian <miladsalami1385@gmail.com>
2026-05-23 17:10:26 +0330

    chack user is ban in middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const { User, Wallet } = require("../models");
+const createError = require("http-errors");
+const { calculateRemainingBanTime } = require("../utils/func");
+const banService = require("../services/banService");
 
 async function authMiddleware(req, res) {
   try {
     let token = req.headers.authorization;
 
-    if (!token)
-      return res
-        .status(401)
-        .send({ statusCode: 401, message: "لطفا توکن را وارد کنید" });
+    if (!token) throw createError.Unauthorized("لطفا توکن را وارد کنید");
 
     token = token.split(" ")[1];
 
@@ -23,11 +23,32 @@ async function authMiddleware(req, res) {
       attributes: { exclude: ["password"] },
     });
 
+    if (!user) {
+      throw createError.NotFound("کاربری با این اطلاعات یافت نشد");
+    }
+
+    const activeBan = await banService.findBanByUserId(user.id);
+
+    if (activeBan) {
+      const banMessage = calculateRemainingBanTime(
+        activeBan.is_permanent,
+        activeBan.expires_at,
+      );
+
+      throw createError.Forbidden(banMessage);
+    }
+
     req.user = user;
   } catch (error) {
-    return res
-      .status(401)
-      .send({ message: "توکن نامعتبر می باشد", statusCode: 401 });
+    if (error.name === "JsonWebTokenError") {
+      throw createError.Unauthorized("توکن نامعتبر می باشد");
+    }
+
+    if (error.name === "TokenExpiredError") {
+      throw createError.Unauthorized("توکن منقضی شده است");
+    }
+
+    throw error;
   }
 }
 
