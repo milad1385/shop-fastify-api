2fe4ebce87f2d4b9045a6ccbcaf0862112feba5b
Milad salamian <miladsalami1385@gmail.com>
2026-05-23 16:30:27 +0330

    add un ban user controller

--- controllers/banController.js
+++ controllers/banController.js
@@ -14,12 +14,20 @@ module.exports = {
       isPermanent,
     );
 
-    return res
-      .status(201)
-      .send({
-        statusCode: 201,
-        message: "کاربر مورد نظر بن شد",
-        data: banedUser,
-      });
+    return res.status(201).send({
+      statusCode: 201,
+      message: "کاربر مورد نظر بن شد",
+      data: banedUser,
+    });
+  },
+  async unBanUser(req, res) {
+    const { userId } = req.body;
+    const unBanedUser = await banService.unBanUserById(userId);
+
+    return res.status(200).send({
+      statusCode: 200,
+      message: "کاربر از بن لیست حذف شد",
+      data: unBanedUser,
+    });
   },
 };
