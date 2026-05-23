361f1094bf1e74e07a317df9cac0e08e8384e2f8
Milad salamian <miladsalami1385@gmail.com>
2026-05-23 17:10:16 +0330

    add calculateRemainingBanTime func in utils

--- utils/func.js
+++ utils/func.js
@@ -1,8 +1,25 @@
 exports.createPagination = (page, limit, totalCount, resourceName) => {
   return {
-    currentPage : page,
+    currentPage: page,
     limit,
     totalPage: Math.ceil(totalCount / limit),
     ["total" + resourceName]: totalCount,
   };
+};
+
+exports.calculateRemainingBanTime = (is_permanent, expires_at) => {
+  if (!is_permanent && expires_at) {
+    const now = new Date();
+    const expiry = new Date(expires_at);
+    const diffMs = expiry - now;
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+    const diffHours = Math.floor((diffMs % 86400000) / (1000 * 60 * 60));
+
+    remainingTime = `${diffDays} روز و ${diffHours} ساعت`;
+  }
+
+  const banMessage = is_permanent
+    ? "حساب کاربری شما به طور دائمی مسدود شده است"
+    : ` حساب کاربری شما تا تاریخ ${new Date(expires_at).toLocaleDateString("fa-IR")} مسدود است`;
+  return banMessage;
 };
