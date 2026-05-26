cd8653fa038c315cb765c29d94219537ba81c5c4
Milad salamian <miladsalami1385@gmail.com>
2026-05-27 00:54:51 +0330

    add categories get route

--- routes/category.js
+++ routes/category.js
@@ -3,6 +3,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const isAdmin = require("../middleware/isAdmin");
 
 function router(fastify, options) {
+  fastify.get("/", categoryController.getCategories);
   fastify.post(
     "/",
     { preHandler: [authMiddleware, isAdmin] },
