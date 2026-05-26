553e4b2b20b1a74217f3b70a9130652c2f041dae
Milad salamian <miladsalami1385@gmail.com>
2026-05-27 00:31:33 +0330

    add delete category by id service and find category by id service

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,6 +1,11 @@
+const createError = require("http-errors");
 const { Category } = require("../models");
 
 module.exports = {
+  async findCategoryById(id) {
+    const category = await Category.findOne({ where: { id } });
+    return category;
+  },
   async createCategory(title, href, description) {
     const newCategory = await Category.create({
       title,
@@ -9,5 +14,14 @@ module.exports = {
     });
 
     return newCategory;
+  },
+  async deleteCategoryById(id) {
+    const category = await this.findCategoryById(id);
+
+    if (!category) {
+      throw createError.NotFound("دسته بندی با این آیدی پیدا نشد");
+    }
+    await category.destroy({});
+    return category;
   },
 };
