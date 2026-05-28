d084e1dd6165fb2bcab71321c4c5d8be22e42325
Milad salamian <miladsalami1385@gmail.com>
2026-05-28 14:49:01 +0330

    add seller model

---
+++ models/Seller.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../configs/db");
+
+const Seller = sequelize.define(
+  "Seller",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postal_code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    province: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = Seller;
