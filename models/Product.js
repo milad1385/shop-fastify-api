e294695eba33bf03491bb5f5269cbed0081e9a63
Milad salamian <miladsalami1385@gmail.com>
2026-05-28 14:48:36 +0330

    add product model

---
+++ models/Product.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../configs/db");
+
+const Product = sequelize.define(
+  "Product",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    href: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    short_identifier: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = Product;
