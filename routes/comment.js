66097aa5c301ec245b22932724ad5c7ec151c863
Milad salamian <miladsalami1385@gmail.com>
2026-05-31 23:57:57 +0330

    add comment route

--- routes/comment.js
+++ routes/comment.js
@@ -3,6 +3,11 @@ const authMiddleware = require("../middleware/authMiddleware");
 const isAdmin = require("../middleware/isAdmin");
 
 function router(fastify, options) {
+  fastify.get(
+    "/",
+    { preHandler: [authMiddleware, isAdmin] },
+    commentController.getComments,
+  );
   fastify.post(
     "/",
     { preHandler: authMiddleware },
