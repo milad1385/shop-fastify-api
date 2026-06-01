888f50c90338febcb98787500c5896a0484807ac
Milad salamian <miladsalami1385@gmail.com>
2026-06-01 18:03:20 +0330

    add add bookmark route

---
+++ routes/bookmark.js
@@ -0,0 +1,13 @@
+const bookmarkController = require("../controllers/bookmarkController");
+const authMiddleware = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/isAdmin");
+
+function router(fastify, options) {
+  fastify.post(
+    "/",
+    { preHandler: [authMiddleware, isAdmin] },
+    bookmarkController.addBookmark,
+  );
+}
+
+module.exports = router;
