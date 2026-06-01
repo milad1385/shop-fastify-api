7d0e243d637c43e103a9cc6a88f2410410cf576b
Milad salamian <miladsalami1385@gmail.com>
2026-06-01 18:10:47 +0330

    add findUserBookmarks controller

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,4 +1,5 @@
 const bookmarkService = require("../services/bookmarkService");
+const { createPagination } = require("../utils/func");
 
 module.exports = {
   async addBookmark(req, res) {
@@ -18,6 +19,27 @@ module.exports = {
       statusCode: 201,
       message: "بوک مارک با موفقیت اضافه شد",
       data: newBookmark,
+    });
+  },
+  async getUserBookmarks(req, res) {
+    const id = req.user.id;
+    let { page, limit, status } = req.query;
+    page = +page || 1;
+    limit = +limit || 10;
+
+    const { bookmarks, count } = await bookmarkService.findUserBookmarksById(
+      id,
+      page,
+      limit,
+    );
+
+    return res.status(200).send({
+      statusCode: 200,
+      message: "بوک مارک های کاربر با موفقیت دریافت شد",
+      data: {
+        bookmarks,
+        pagination: createPagination(page, limit, count, "Bookmarks"),
+      },
     });
   },
 };
