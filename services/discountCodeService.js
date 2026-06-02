6af26cfc0ccaefc45b5045e7e1b6c51ca91fae4d
Milad salamian <miladsalami1385@gmail.com>
2026-06-02 13:54:34 +0330

    add getDiscounts service

--- services/discountCodeService.js
+++ services/discountCodeService.js
@@ -5,6 +5,15 @@ module.exports = {
   async findDiscountByCode(code) {
     return await DiscountCode.findOne({ where: { code } });
   },
+  async findAllDiscount(page = 1, limit = 10) {
+    const count = await DiscountCode.count({});
+    const discounts = await DiscountCode.findAll({
+      limit,
+      offset: (page - 1) * limit,
+    });
+
+    return { count, discounts };
+  },
   async createDiscountCode(discountCode) {
     const { code, capacity, off_percent } = discountCode;
     const discount = await this.findDiscountByCode(code);
