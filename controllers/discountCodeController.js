69348033fc88c637e6112d95ec3cc90dce29f6ea
Milad salamian <miladsalami1385@gmail.com>
2026-06-02 13:54:28 +0330

    add getDiscounts controller

--- controllers/discountCodeController.js
+++ controllers/discountCodeController.js
@@ -1,6 +1,25 @@
 const discountCodeService = require("../services/discountCodeService");
+const { createPagination } = require("../utils/func");
 
 module.exports = {
+  async getDiscounts(req, res) {
+    let { page, limit } = req.query;
+    page = +page || 1;
+    limit = +limit || 10;
+    const { count, discounts } = await discountCodeService.findAllDiscount(
+      page,
+      limit,
+    );
+
+    return res.status(200).send({
+      statusCode: 200,
+      message: "لیست کد های تخفیف با موفقیت دریافت شد",
+      data: {
+        discounts,
+        pagination: createPagination(page, limit, count, "Discount"),
+      },
+    });
+  },
   async addDiscountCode(req, res) {
     const newDiscount = await discountCodeService.createDiscountCode(req.body);
     return res.status(201).send({
