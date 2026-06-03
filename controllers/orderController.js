626dd7f2c701973c83326decf44ad770d00d2a49
Milad salamian <miladsalami1385@gmail.com>
2026-06-04 00:14:13 +0330

    add getUserOrders controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,16 +1,36 @@
 const orderService = require("../services/orderService");
+const { createPagination } = require("../utils/func");
 
 module.exports = {
   async addNewOrder(req, res) {
     const userId = req.user.id;
     const newOrder = await orderService.createOrder(userId);
 
-    return res
-      .status(201)
-      .send({
-        statusCode: 201,
-        message: "سفارش کاربر با موفقیت ثبت شد",
-        data: newOrder,
-      });
+    return res.status(201).send({
+      statusCode: 201,
+      message: "سفارش کاربر با موفقیت ثبت شد",
+      data: newOrder,
+    });
+  },
+  async getUserOrders(req, res) {
+    let { page, limit, status } = req.query;
+    page = +page || 1;
+    limit = +limit || 10;
+    const userId = req.user.id;
+    const { orders, count } = await orderService.getUserOrdersById(
+      userId,
+      status,
+      page,
+      limit,
+    );
+
+    return res.status(200).send({
+      statusCode: 200,
+      message: "سفارشات کاربر با موفقیت دریافت شد",
+      data: {
+        orders,
+        pagination: createPagination(page, limit, count, "Orders"),
+      },
+    });
   },
 };
