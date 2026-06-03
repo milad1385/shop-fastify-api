db3ce3e0a084ee4f91684aa1d86335c5f50b3a95
Milad salamian <miladsalami1385@gmail.com>
2026-06-03 15:15:17 +0330

    add basket model

---
+++ models/Basket.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../configs/db");
+
+const Basket = sequelize.define(
+  "Basket",
+  {
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    price: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    discount: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = Basket;
