94ef4bf689dfe60d83f3e765a82f6c3138e3bb48
Milad salamian <miladsalami1385@gmail.com>
2026-06-03 16:32:56 +0330

    add basket route

---
+++ routes/basket.js
@@ -0,0 +1,12 @@
+const banController = require("../controllers/banController");
+const basketController = require("../controllers/basketController");
+const authMiddleware = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/isAdmin");
+
+function router(fastify, options) {
+  fastify.post(
+    "/",
+    { preHandler: authMiddleware },
+    basketController.addProductToBasket,
+  );
+}
