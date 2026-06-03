141f1fb64f931feccdfb0745930b4f20c86f03d3
Milad salamian <miladsalami1385@gmail.com>
2026-06-03 16:59:11 +0330

    add findBasketById service

--- services/basketService.js
+++ services/basketService.js
@@ -3,6 +3,11 @@ const { Basket, ProductSeller } = require("../models");
 const productService = require("./productService");
 
 module.exports = {
+  async findBasketById(id) {
+    const basket = await Basket.findOne({ where: { id } });
+
+    return basket;
+  },
   async createBasket(userId, basketInfo) {
     const { product_id, seller_id } = basketInfo;
     const product = await productService.findProductById(product_id);
@@ -11,19 +16,19 @@ module.exports = {
       throw createError.NotFound("محصولی با این آیدی یافت نشد");
     }
 
-    const sellerInfo = await ProductSeller.findOne({ seller_id, product_id });
+    const sellerInfo = await ProductSeller.findOne({
+      where: { seller_id, product_id },
+    });
     if (!sellerInfo) {
       throw createError.NotFound("فروشنده ای با این آیدی یافت نشد");
     }
 
     let basket = await Basket.findOne({
-      product_id,
-      seller_id,
-      user_id: userId,
+      where: { product_id, seller_id, user_id: userId },
     });
 
     if (basket) {
-      if (basket.quantity > sellerInfo.stock) {
+      if (basket.quantity >= sellerInfo.stock) {
         throw createError.BadRequest("تعداد محصول بیشتر از موجودی فروشنده است");
       }
       basket = await basket.increment("quantity", {
@@ -40,6 +45,6 @@ module.exports = {
       });
     }
 
-    return basket;
+    return await this.findBasketById(basket.id);
   },
 };
