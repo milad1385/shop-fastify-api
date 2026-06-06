ec79dcf538c8f9a3e33ab4017593e9aba70c1bc2
Milad salamian <miladsalami1385@gmail.com>
2026-06-06 22:12:52 +0330

    fix createNewMenu service bug

--- services/menuService.js
+++ services/menuService.js
@@ -10,14 +10,14 @@ module.exports = {
     return menu;
   },
   async createNewMenu(menuData) {
-    const { title, href } = menuData;
+    const { title, href, parentId } = menuData;
     const menu = await this.findMenuByTitleAndTitle(title, href);
     if (menu) {
       throw createError.BadRequest("منو با این مشخصات وجود دارد");
     }
 
-    const newMenu = await Menu.create({ title, href });
+    const newMenu = await Menu.create({ title, href, parent_id: parentId });
 
-    return newMenu;
+    return await this.findMenuByTitleAndTitle(title, href);
   },
 };
