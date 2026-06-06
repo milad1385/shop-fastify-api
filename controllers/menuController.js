47af154e79a0001ca481c36e52c09a19abb3f7eb
Milad salamian <miladsalami1385@gmail.com>
2026-06-06 22:21:14 +0330

    add find all menus controller

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,4 +1,5 @@
 const menuService = require("../services/menuService");
+const { createPagination } = require("../utils/func");
 
 module.exports = {
   async addNewMenu(req, res) {
@@ -8,5 +9,18 @@ module.exports = {
       .status(201)
       .send({ statusCode: 201, message: "منو با موفقیت ساخته شد", data: menu });
   },
-  
+  async findAllMenus(req, res) {
+    let { page, limit } = req.query;
+    page = +page || 1;
+    limit = +limit || 10;
+    const { count, menus } = await menuService.findAllMenus(page, limit);
+
+    return res
+      .status(200)
+      .send({
+        statusCode: 200,
+        message: "منو ها با موفقیت دریافت شد",
+        data: createPagination(page, limit, count, "Menus"),
+      });
+  },
 };
