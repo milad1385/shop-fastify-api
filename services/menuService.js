a7caf236a5249a6a745714e7e18ce080a0f54849
Milad salamian <miladsalami1385@gmail.com>
2026-06-06 21:50:54 +0330

    add createNewMenu and findMenuByTitleAndTitle controller

---
+++ services/menuService.js
@@ -0,0 +1,23 @@
+const { Op } = require("sequelize");
+const { Menu } = require("../models");
+const createError = require("http-errors");
+
+module.exports = {
+  async findMenuByTitleAndTitle(title, href) {
+    const menu = await Menu.findOne({
+      where: { [Op.or]: [{ title }, { href }] },
+    });
+    return menu;
+  },
+  async createNewMenu(menuData) {
+    const { title, href } = menuData;
+    const menu = await this.findMenuByTitleAndTitle(title, href);
+    if (menu) {
+      throw createError.BadRequest("منو با این مشخصات وجود دارد");
+    }
+
+    const newMenu = await Menu.create({ title, href });
+
+    return newMenu;
+  },
+};
